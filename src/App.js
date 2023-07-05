86279b448aa0cd69ba178efa4805f78eb6457505
Rohit Sanaka <srohitkumar147@gmail.com>
2023-07-05 21:18:23 +0530

    ghjm,

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import '/Index.css';
 
 import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
 
-import AppLayout from '../src/components/AppLayout';
-import Main from '../src/components/Main';
-import Offers from '../src/components/Offers';
-import Help from '../src/components/Help';
+import AppLayout from './components/AppLayout';
+import Main from './components/Main';
+import Offers from './components/Offers';
+import Help from './components/Help';
 // import ProfileFunctional from "../src/components/Profile";
-import ProfileClass from '../src/components/ProfileClass';
-import Cart from '../src/components/Cart';
-import ErrorPage from '../src/components/ErrorPage';
-import RestaurantMenu from '../src/components/RestaurantMenu';
+import ProfileClass from './components/ProfileClass';
+import Cart from './components/Cart';
+import ErrorPage from './components/ErrorPage';
+import RestaurantMenu from './components/RestaurantMenu';
 
 console.log('inside app');
 const router = createBrowserRouter([
